2fb2f2a0fcffed8d4bbb307a4df26f3751864963
Serednii Mykola <seredniimykola@gmail.com>
2024-07-27 16:22:59 +0200

    deleted addCategoryMain component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,26 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from "react";
 import Header from "./Header/Header";
 import UsefulLinks from "./UsefulLinks/UsefulLinks";
-import AddCategoryMain from "./AddCategoryMain/AddCategoryMain";
 import AddCategoryOther from "./AddCategoryOther/AddCategoryOther";
 import ChangeLinks from "./ChangeLinks/ChangeLinks";
-import { Button } from 'react-bootstrap';
-import { getData, outDataServer } from '../functions/requestHelpers';
+import { Button } from "react-bootstrap";
+import { getData, outDataServer } from "../functions/requestHelpers";
 import Context from "../Context";
-import '../App.css';
+import "../App.css";
 
-import { DATA_MENU, DATA_LINKS } from '../State';
-import { Footer } from './Footer/Footer';
-export const URL_SERVER = 'http://smm.zzz.com.ua/api/api.php';
-
-
+import { DATA_MENU, DATA_LINKS } from "../State";
+import { Footer } from "./Footer/Footer";
+export const URL_SERVER = "http://smm.zzz.com.ua/api/api.php";
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isButtonPlus, setIsButtonPlus] = useState(false);
   const [isChangeLinks, setIsChangeLinks] = useState(false);
-  const [isAddCategoryMain, setIsAddCategoryMain] = useState(false);
   const [isAddCategoryOther, setIsAddCategoryOther] = useState(false);
   const [isChangeLink, setIsChangeLink] = useState(false);
   const [isModal, setIsModal] = useState(false);
 
-  const tempDataRef = useRef<{ test: string; } | null>(null);
+  const tempDataRef = useRef<{ test: string } | null>(null);
   const [dataMain, setDataMain] = useState<any>({});
   const [openAddCategory, setOpenAddCategory] = useState(false);
   const [listLinkData, setListLinkData] = useState<any[]>([]);
@@ -35,13 +31,10 @@ const App: React.FC = () => {
     DATA_LINKS,
     dataMain,
     setDataMain,
-    // dataMenu: DATA_MENU,
     openAddCategory,
     setOpenAddCategory,
     listLinkData,
     setListLinkData,
-    isAddCategoryMain,
-    setIsAddCategoryMain,
     isAddCategoryOther,
     setIsAddCategoryOther,
     isButtonPlus,
@@ -55,8 +48,8 @@ const App: React.FC = () => {
     setIsChangeLinks,
     sluiceLinks,
     isModal,
-    setIsModal
-  }
+    setIsModal,
+  };
 
   const handlerGetDate = (): void => {
     getData(URL_SERVER, setIsLoading)
@@ -65,43 +58,41 @@ const App: React.FC = () => {
         console.log(tempDataRef.current);
       })
       .catch((error: Error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         // throw new Error(String(error));
       });
-  }
+  };
 
   function handlerPostDate() {
-
     tempDataRef.current = {
       ...tempDataRef.current,
-      test: '123'
-    }
+      test: "123",
+    };
 
-    console.log(tempDataRef.current)
-    outDataServer(URL_SERVER, 'PUT', tempDataRef.current);
+    console.log(tempDataRef.current);
+    outDataServer(URL_SERVER, "PUT", tempDataRef.current);
   }
 
   useEffect(() => {
-    console.log('render')
+    console.log("render");
     getData(URL_SERVER, setIsLoading)
       .then((data: any) => {
         setDataMain(data);
       })
       .catch((error: Error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         // throw new Error(String(error));
       });
   }, []);
 
   return (
-    <Context.Provider value={value} >
+    <Context.Provider value={value}>
       <div className="App vh-100 container-xxl d-flex flex-column justify-content-between">
-        <main className='flex-grow-1 d-flex flex-column'>
-
+        <main className="flex-grow-1 d-flex flex-column">
           {isLoading && <h1>Loading...</h1>}
           <Header />
           <UsefulLinks />
-          {isAddCategoryMain && <AddCategoryMain />}
+          {/* {isAddCategoryMain && <AddCategoryMain />} */}
           {isAddCategoryOther && <AddCategoryOther />}
           {isChangeLinks && <ChangeLinks />}
           {/* <Button onClick={handlerGetDate}>Get Data</Button>
@@ -111,6 +102,6 @@ const App: React.FC = () => {
       </div>
     </Context.Provider>
   );
-}
+};
 
 export default App;
