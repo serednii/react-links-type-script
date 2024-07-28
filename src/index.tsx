fc549cde7d082ccbd43a543259554e5f4779a644
Serednii Mykola <seredniimykola@gmail.com>
2024-07-28 08:11:39 +0200

    add MyProvider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'reset-css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
-import App from './components/App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "reset-css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
+import App from "./components/App";
+import { MyProvider } from "./MyContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MyProvider>
+      <App />
+    </MyProvider>
   </React.StrictMode>
 );
-
