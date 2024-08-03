72fc5fff717b8c1fd3fa3df48a0d36be6912dffd
Serednii Mykola <seredniimykola@gmail.com>
2024-08-03 19:01:56 +0200

    delete pages

--- src/pages/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-const Homepage=()=>{
-    return (
-        <div>
-            <h1>Homepage us</h1>
-            <p>This is a demo website about React-router-dom library.</p>
-        </div>
-    )
-}
-export {Homepage}
--- src/pages/Notfoundpage.jsx
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-  return (
-    <div>
-      <h1>NOTFOUNDPAGE</h1>
-      This page dosen't exits. Go <a href="/">home</a>
-    </div>
-  );
-};
-export { NotFoundPage };
