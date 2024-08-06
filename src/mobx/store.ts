a12aed9abb4ed7aa3e12d1f21dfb93e4769542d7
Serednii Mykola <seredniimykola@gmail.com>
2024-08-06 20:12:46 +0200

    add mobx store asynk function

--- src/mobx/store.ts
+++ src/mobx/store.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable } from "mobx";
-
+import graphQLQuery from  '../functions/requestGraphQLClass'
 class DataStore {
     dataMain:  any;
     listLinkData: any;
@@ -11,12 +11,112 @@ class DataStore {
     setDataMain(dataMain: any) {
         this.dataMain = dataMain;
     }
-    setListLinkData(listLinkData: any) {
+    setListLinkData(listLinkData: any) { 
         this.listLinkData = listLinkData
     }
     setSluice(sluice: any) {
         this.sluice = sluice;
     }
+
+    async getMenu() {
+        try {
+          const response = await graphQLQuery.getDataGraphQLMenu();
+          this.dataMain = response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async updateMenu(menu: object) {
+        try {
+          const response = await graphQLQuery.updateDataGraphQLMenu(menu);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async getLink(id: string) {
+        try {
+          const response = await graphQLQuery.getDataGraphQLLink(id);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async addLink(link: string) {
+        try {
+          const response = await graphQLQuery.addDataGraphQLLink(link);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async updateLink(id: string, link: string) {
+        try {
+          const response = await graphQLQuery.updateDataGraphQLLink(id, link);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async deleteLink(id: string) {
+        try {
+          const response = await graphQLQuery.deleteDataGraphQLLink(id);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async getArticle(id: string) {
+        try {
+          const response = await graphQLQuery.getDataGraphQLLink(id);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async addArticle(article: string) {
+        try {
+          const response = await graphQLQuery.addDataGraphQLArticle(article);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async updateArticle(id: string, article: string) {
+        try {
+          const response = await graphQLQuery.updateDataGraphQLArticle(id, article);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
+
+      async deleteArticle(id: string) {
+        try {
+          const response = await graphQLQuery.deleteDataGraphQLLink(id);
+          return  response
+        } catch (error) {
+          console.log(error);
+          throw error
+        }
+      }
 
 }
 
