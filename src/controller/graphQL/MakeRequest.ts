60c64ec1227832704e4734c764a9e026213d4397
Serednii Mykola <seredniimykola@gmail.com>
2024-08-16 22:08:24 +0200

    add token to MakeRequest

--- src/controller/graphQL/MakeRequest.ts
+++ src/controller/graphQL/MakeRequest.ts
@@ -1,44 +1,53 @@
 import { URL } from '../../const';
 
-
 class MakeRequest {
-    isLoading = false;
-    data: { method: string; headers: { "Content-Type": string }; body?: string };
-    url: string;
-  
-    constructor() {
-      this.url = URL;
-      this.data = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-    }
-  
-    request = async (query: string) => {
-      this.data.body = JSON.stringify({ query });
-  
-      try {
-        this.isLoading = true;
-        const response = await fetch(this.url, { ...this.data });
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-  
-        const result = await response.json();
-        return result;
-      } catch (error) {
-        console.error("Fetch error:", error);
-        throw error;
-      } finally {
-        this.isLoading = false;
-      }
+  isLoading = false;
+  data: { method: string; headers: { "Content-Type": string; Authorization?: string }; body?: string };
+  url: string;
+
+  constructor() {
+    this.url = URL;
+    this.data = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
-  
-    isLoadingStatus() {
-      return this.isLoading;
+  }
+
+  setAuthToken() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.data.headers.Authorization = `Bearer ${token}`;
     }
   }
 
-  export default MakeRequest;
+  request = async (query: string) => {
+    this.data.body = JSON.stringify({ query });
+    
+    // Додаємо Bearer токен перед запитом
+    this.setAuthToken();
+
+    try {
+      this.isLoading = true;
+      const response = await fetch(this.url, { ...this.data });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Fetch error:", error);
+      throw error;
+    } finally {
+      this.isLoading = false;
+    }
+  };
+
+  isLoadingStatus() {
+    return this.isLoading;
+  }
+}
+
+export default MakeRequest;
