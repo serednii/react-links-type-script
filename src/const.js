7171588b4c00023e62c5f2c601fe8d759b50e15a
Serednii Mykola <seredniimykola@gmail.com>
2024-08-21 21:02:00 +0200

    change Url to BASE_URL

--- src/const.js
+++ src/const.js
@@ -1,4 +1,8 @@
 export const URL_SERVER = "http://smm.zzz.com.ua/api/api.php";
 // export const PASSWORD = "text code"
-export const URL = "http://localhost:7000/api/graphql/";
-// export const URL = "https://serverlinkgraphql-production.up.railway.app/api/graphql/";
+// export const BASE_URL = "http://localhost:7000/api/graphql";
+export const BASE_URL = "https://jwtserver-production.up.railway.app"
+
+export const URL_GRAPHQL = BASE_URL + "/api/graphql";
+export const URL_AUTH = BASE_URL + "api";
+// API_URL
