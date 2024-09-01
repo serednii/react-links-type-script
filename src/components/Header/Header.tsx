71cac6134608ad32b8f8d8fb69262233efd3f423
Serednii Mykola <seredniimykola@gmail.com>
2024-09-01 07:46:28 +0200

    change  images in scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,7 @@ import AuthUser from "../../AuthUser/components/AuthUser/AuthUser";
 // import ButtonsAdd from "./ButtonsAdd/ButtonsAdd111";
 import authStore from "../../mobx/AuthStore";
 import AuthForms from "../../AuthUser/components/AuthForms/AuthForms";
-import usefulLinksImg from "../../images/5326787.png";
-import usefulLinksTitle from "../../images/Useful+Links.png";
+// import usefulLinksImg from "../../images/5326787.png";
 
 import "./Header.scss";
 
@@ -13,17 +12,9 @@ const Header: React.FC = () => {
 
   return (
     <header className="header mb-5   bg-opacity-75 rounded-4 p-3">
-      {/* <div className="row md-3">
-        <div className="col-12">
-          <h1 className="header__title text-uppercase ">Useful links</h1>
-        </div>
-      </div> */}
       <div className="header__top">
-        <img src={usefulLinksImg} className="logo" alt="useful_links" />
+        <img src="../images/5326787.png" className="logo" alt="useful_links" />
 
-        {/* <div>
-          <img src={usefulLinksTitle} className="title" alt="useful_links" />
-        </div> */}
         <h1 className="header__title text-uppercase ">Useful links</h1>
         {!authStore.isAuth ? <AuthForms /> : <AuthUser />}
       </div>
